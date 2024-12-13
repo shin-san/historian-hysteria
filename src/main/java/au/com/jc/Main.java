c88802d85ed1e1d7f6330a99dc92605d68bd424c
JC <jchan@integral.com.au>
2024-12-13 16:57:42 +1000

    Initial commit - day 1

---
+++ src/main/java/au/com/jc/Main.java
@@ -0,0 +1,53 @@
+package au.com.jc;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> location1 = new ArrayList<>();
+        List<Integer> location2 = new ArrayList<>();
+
+        final Path filePath = new File("src/main/resources/locations.txt").toPath();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(filePath), StandardCharsets.UTF_8))) {
+            String line;
+
+            // read it from a .txt file
+            while((line = br.readLine()) != null) {
+                var locations = line.split("   ");
+                location1.add(Integer.valueOf(locations[0]));
+                location2.add(Integer.valueOf(locations[1]));
+            }
+
+            // sort both arrays
+            List<Integer> sortedLocation1 = location1.stream().sorted().toList();
+            List<Integer> sortedLocation2 = location2.stream().sorted().toList();
+
+            var totalDistance = 0L;
+
+            for (int i = 0; i < sortedLocation1.size(); i++) {
+                var distance = 0L;
+                if (sortedLocation1.get(i) > sortedLocation2.get(i)) {
+                    distance =  sortedLocation1.get(i) - sortedLocation2.get(i);
+                } else {
+                    distance = sortedLocation2.get(i) - sortedLocation1.get(i);
+                }
+                totalDistance += distance;
+            }
+
+            System.out.println(totalDistance);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
